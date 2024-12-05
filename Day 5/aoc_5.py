930d0d698ef8ffec76dd4958b0bf879cfb242962
Cago2000 <147174299+Cago2000@users.noreply.github.com>
2024-12-05 19:30:29 +0100

    Day 5 done

--- Day 5/aoc_5.py
+++ Day 5/aoc_5.py
@@ -1,12 +1,41 @@
-page_ruling = open('page_ruling.txt', 'r')
-page_ruling_list = {}
-for line in page_ruling:
-    X, Y = map(int, line.strip().split('|'))
-    if X not in page_ruling_list:
-        page_ruling_list[X] = []
-    if Y not in page_ruling_list:
-        page_ruling_list[Y] = []
-    page_ruling_list[X].append(Y)
+def get_page_ruling_list():
+    page_ruling = open('page_ruling.txt', 'r')
+    page_ruling_list = {}
+    for line in page_ruling:
+        X, Y = map(int, line.strip().split('|'))
+        if X not in page_ruling_list:
+            page_ruling_list[X] = []
+        if Y not in page_ruling_list:
+            page_ruling_list[Y] = []
+        page_ruling_list[X].append(Y)
+    return page_ruling_list
+
+
+def check_updates(updates):
+    is_valid = True
+    for i, _ in enumerate(updates):
+        if not set(updates[i+1:]).issubset(set(pr_list[updates[i]])):
+            is_valid = False
+            break
+    return is_valid
+
+
+def correct_invalid_updates(updates):
+    i = 0
+    while i < len(updates)-1:
+        if not set(updates[i+1:]).issubset(set(pr_list[updates[i]])):
+            issues = [issue for issue in updates[i+1:] if issue not in pr_list[updates[i]]]
+            for issue in issues:
+                j = updates.index(issue)
+                updates[i], updates[j] = updates[j], updates[i]
+                i = j
+            i = 0
+        else:
+            i += 1
+    return updates
+
+
+pr_list = get_page_ruling_list()
 
 
 def main():
@@ -26,42 +55,10 @@ def main():
     print(middle_page_sum)
 
     middle_page_sum2 = 0
-    counter = 0
-    print(len(invalid_updates_list))
-    for updates in invalid_updates_list:
-        updates = correct_invalid_updates(updates)
-        is_valid = check_updates(updates)
-        if is_valid:
-            counter += 1
-            middle_page_sum2 += updates[int(len(updates)/2)]
-    print(middle_page_sum+middle_page_sum2, counter)
-
-
-
-def check_updates(updates):
-    is_valid = True
-    for i, update in enumerate(updates):
-        if not set(updates[i+1:]).issubset(set(page_ruling_list[update])):
-            is_valid = False
-            break
-    return is_valid
-
-
-def correct_invalid_updates(updates):
-    print(updates)
-    is_valid = True
-    for i, update in enumerate(updates):
-        if not set(updates[i+1:]).issubset(set(page_ruling_list[update])):
-            issues = [issue for issue in updates[i+1:] if issue not in page_ruling_list[update]]
-            print(f'{updates[i]}, {issues}')
-            cur_index = i
-            for j, _ in enumerate(updates[cur_index+1:]):
-                if updates[cur_index+j] in issues:
-                    updates[cur_index], updates[cur_index+1+j] = updates[cur_index+1+j], updates[cur_index]
-                    cur_index += 1
-    print(updates)
-    print()
-    return updates
+    for i, invalid_updates in enumerate(invalid_updates_list):
+        invalid_updates_list[i] = correct_invalid_updates(invalid_updates)
+        middle_page_sum2 += invalid_updates[int(len(invalid_updates)/2)]
+    print(middle_page_sum2)
 
 
 if __name__ == "__main__":
