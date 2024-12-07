33356c046354db91d6de16089a704376e2629e2e
Cago2000 <147174299+Cago2000@users.noreply.github.com>
2024-12-07 02:31:46 +0100

    Day 6 done

--- Day 6/aoc_6.py
+++ Day 6/aoc_6.py
@@ -1,14 +1,21 @@
-input_map = open('map.txt', 'r')
-map_list = []
+import copy
 directions = {"^": (-1, 0), ">": (0, 1), "v": (1, 0), "<": (0, -1)}
-current_index = 0
+dir_index = 0
+loop_counter = 0
 
 
-def get_next_dir():
-    global current_index
-    current_index += 1
-    current_index %= 4
-    return list(directions)[current_index]
+def get_next_dir_key():
+    global dir_index
+    dir_index += 1
+    dir_index %= 4
+    return list(directions)[dir_index]
+
+
+def get_next_dir_value():
+    global dir_index
+    dir_index += 1
+    dir_index %= 4
+    return directions[list(directions)[dir_index]]
 
 
 def get_key_by_value(searched_value):
@@ -17,55 +24,99 @@ def get_key_by_value(searched_value):
             return key
 
 
-def walk():
-    i, j = get_current_position()
-    a, b = get_current_direction()
-    if i+a >= len(map_list) or i+a < 0 or j+b >= len(map_list[i]) or j+b < 0:
-        return True
-    if map_list[i+a][j+b] == '#':
-        dir = get_next_dir()
-        map_list[i][j] = dir
-        return False
-    map_list[i+a][j+b] = map_list[i][j]
-    map_list[i][j] = 'X'
-    return False
-
-
-def walk_to_end():
-    done = False
-    while not done:
-        done = walk()
-    print("End reached!")
-    count = 0
-    for line in map_list:
-        count += line.count('X')
-        print(line)
-    count += 1 #Last position
-    print(count)
-
-
-
-
-
-def get_current_position():
+def get_current_position(map_list):
     for i, line_list in enumerate(map_list):
         for j, char in enumerate(line_list):
             if char != '.' and char != '#' and char != 'X':
                 return i, j
 
 
-def get_current_direction():
-    i, j = get_current_position()
+def get_current_direction(map_list):
+    i, j = get_current_position(map_list)
     return directions[map_list[i][j]]
 
 
+def walk(map_list):
+    global loop_counter
+    i, j = get_current_position(map_list)
+    a, b = get_current_direction(map_list)
+    if i+a >= len(map_list) or i+a < 0 or j+b >= len(map_list[i]) or j+b < 0:
+        map_list[i][j] = 'X'
+        return True
+    if map_list[i+a][j+b] == '#':
+        next_dir = get_next_dir_key()
+        map_list[i][j] = next_dir
+        return False
+    map_list[i+a][j+b] = map_list[i][j]
+    map_list[i][j] = 'X'
+    return False
+
+
+def walk_to_end(map_list, i, j):
+    global dir_index
+    states = []
+    done = False
+    while not done:
+        done= walk(map_list)
+    map_list[i][j] = 'X'
+    dir_index = 0
+    count = 0
+    for line in map_list:
+        count += line.count('X')
+    return count
+
+
+def check_loop(map_list, states):
+    global loop_counter
+    i, j = states[-1][0]
+    a, b = states[-1][1]
+    if i+a >= len(map_list) or i+a < 0 or j+b >= len(map_list[i]) or j+b < 0:
+        return True, states
+    if map_list[i+a][j+b] == '#':
+        (a, b) = get_next_dir_value()
+    else:
+        i = i+a
+        j = j+b
+    if ((i, j), (a, b)) in states:
+        loop_counter += 1
+        return True, states
+    else:
+        states.append(((i, j), (a, b)))
+    return False, states
+
+
+def check_loop_to_end(map_list, i, j):
+    states = [((i, j), (-1, 0))]
+    done = False
+    while not done:
+        done, states = check_loop(map_list, states)
+
+
 def main():
+    path = 'map.txt'
+    input_map = open(path, 'r')
+    map_list = []
     for line in input_map:
         line_list = []
         for char in line.strip():
             line_list.append(char)
         map_list.append(line_list)
-    walk_to_end()
+    x, y = get_current_position(map_list)
+    counter = walk_to_end(map_list, x, y)
+    print(f'Part One: Counter: {counter}')
+    map_list_copy = copy.deepcopy(map_list)
+    global dir_index
+    iter_counter = 0
+    for i, line_list in enumerate(map_list_copy):
+        for j, char in enumerate(line_list):
+            if char == 'X':
+                print(iter_counter)
+                iter_counter += 1
+                map_list_copy[i][j] = '#'
+                check_loop_to_end(map_list_copy, x, y)
+                map_list_copy[i][j] = 'X'
+                dir_index = 0
+    print(f'loop_counter: {loop_counter}')
 
 
 if __name__ == "__main__":
