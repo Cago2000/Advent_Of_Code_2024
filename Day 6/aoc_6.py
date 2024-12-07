14e6e72f04f5ebd2bd4bd9cbdf9b9ee52f44f842
Cago2000 <147174299+Cago2000@users.noreply.github.com>
2024-12-07 02:47:29 +0100

    Improved speed by using set

--- Day 6/aoc_6.py
+++ Day 6/aoc_6.py
@@ -1,4 +1,6 @@
 import copy
+from collections import OrderedDict
+
 directions = {"^": (-1, 0), ">": (0, 1), "v": (1, 0), "<": (0, -1)}
 dir_index = 0
 loop_counter = 0
@@ -66,12 +68,10 @@ def walk_to_end(map_list, i, j):
     return count
 
 
-def check_loop(map_list, states):
+def check_loop(map_list, states, i, j, a, b):
     global loop_counter
-    i, j = states[-1][0]
-    a, b = states[-1][1]
     if i+a >= len(map_list) or i+a < 0 or j+b >= len(map_list[i]) or j+b < 0:
-        return True, states
+        return True, states, i, j, a, b
     if map_list[i+a][j+b] == '#':
         (a, b) = get_next_dir_value()
     else:
@@ -79,17 +79,19 @@ def check_loop(map_list, states):
         j = j+b
     if ((i, j), (a, b)) in states:
         loop_counter += 1
-        return True, states
+        return True, states, i, j, a, b
     else:
-        states.append(((i, j), (a, b)))
-    return False, states
+        states.add(((i, j), (a, b)))
+    return False, states, i, j, a, b
 
 
 def check_loop_to_end(map_list, i, j):
-    states = [((i, j), (-1, 0))]
+    states = set()
+    a, b = -1, 0
+    states.add(((i, j), (-1, 0)))
     done = False
     while not done:
-        done, states = check_loop(map_list, states)
+        done, states, i, j, a, b = check_loop(map_list, states, i, j, a, b)
 
 
 def main():
@@ -116,7 +118,7 @@ def main():
                 check_loop_to_end(map_list_copy, x, y)
                 map_list_copy[i][j] = 'X'
                 dir_index = 0
-    print(f'loop_counter: {loop_counter}')
+    print(f'Part Two: Loop Counter: {loop_counter}')
 
 
 if __name__ == "__main__":
