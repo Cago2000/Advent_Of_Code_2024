f8d2df1276aff9bc97cb3b9ab6a537c5884d7f39
Cago2000 <147174299+Cago2000@users.noreply.github.com>
2025-08-02 02:09:19 +0200

    cleaned up version using reduce function

--- Day 7/aoc7.py
+++ Day 7/aoc7.py
@@ -1,37 +1,39 @@
+import functools
 import itertools
 from enum import Enum
+from typing import Tuple
 
 class Operator(Enum):
     ADD = 0
     MULTIPLY = 1
     CONCAT = 2
-    def calculate(self, a: int, b: int):
+    def calculate(self, a: int, b: int) -> int:
         match self:
-            case Operator.ADD:
-                return a + b
-            case Operator.MULTIPLY:
-                return a * b
+            case Operator.ADD: return a + b
+            case Operator.MULTIPLY: return a * b
             case Operator.CONCAT:
                 a, b = str(a), str(b)
                 return int(a+b)
 
+def evaluate(acc: int, operator_value_pair: Tuple[Operator, int]) -> int:
+    operator, next_val = operator_value_pair
+    return operator.calculate(acc, next_val)
+
 def main():
     all_operators = list(Operator)
-    path = 'calibration_equations.txt'
-    calibration_equations_file = open(path, 'r')
+    calibration_equations_file = open('calibration_equations_example.txt', 'r')
     equations = {}
 
     for equation_id, line in enumerate(calibration_equations_file):
         elements = line.split()
-        equation_result = int(elements[0][:-1])
-        equation_term = list(map(int, elements[1:]))
         equation = {
-            "result": equation_result,
-            "terms": equation_term,
+            "result": int(elements[0][:-1]),
+            "terms": list(map(int, elements[1:])),
             "operators": []
         }
         equations[equation_id] = equation
 
+    true_equations = []
     total_sum = 0
     for equation_id in equations:
         equation = equations[equation_id]
@@ -40,24 +42,19 @@ def main():
         is_equation_true = False
         cur_result = 0
         for operators in operator_combinations:
-            cur_result = 0
-            term_pairs = itertools.pairwise(equation["terms"])
-            first_pair = True
-            for (term_pair, operator) in zip(term_pairs, operators):
-                a, b = term_pair
-                if not first_pair:
-                    a = cur_result
-                else:
-                    first_pair = False
-
-                cur_result = operator.calculate(a, b)
-
+            cur_result = functools.reduce(evaluate, zip(operators, equation["terms"][1:]), equation["terms"][0])
             if cur_result == equation["result"]:
+                equation["operators"] = operators
+                true_equations.append(equation)
                 is_equation_true = True
                 break
         if is_equation_true:
             total_sum += cur_result
 
     print(f'total sum: {total_sum}')
+    print(f'true equations:')
+    for equation in true_equations:
+        print(equation)
+
 if __name__ == "__main__":
     main()
