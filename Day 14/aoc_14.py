152266f320169ceb47ebf126a6b693d97b38d59d
Cago2000 <147174299+Cago2000@users.noreply.github.com>
2025-08-11 00:09:52 +0200

    Day 14 part 1 done

--- Day 14/aoc_14.py
+++ Day 14/aoc_14.py
@@ -9,24 +9,55 @@ class Robot:
     velocity: tuple[int, int]
 
 def main():
-    robots_data = load_file("robots_example.txt")
+    robots_data = load_file("robots.txt")
     robots = get_robot_data(robots_data)
-    width, height = 11, 7
-    draw_tiles(robots, width, height)
+    width, height = 101, 103
     seconds = 100
-    tiles = []
     for _ in range(seconds):
         robots = move_robots(robots, width, height)
-        tiles =draw_tiles(robots, width, height)
-    calculate_safety_score(tiles)
+    tiles = draw_tiles(robots, width, height)
+    safety_score = calculate_safety_score(tiles)
+    print(safety_score)
+
+    '''
+    seconds = 0
+    safety_score_changes = True
+    safety_scores = set()
+    while safety_score_changes:
+        seconds += 1
+        robots = move_robots(robots, width, height)
+        tiles = draw_tiles(robots, width, height)
+        safety_score = calculate_safety_score(tiles)
+        if safety_score in safety_scores:
+            safety_score_changes = False
+        else:
+            safety_scores.add(safety_score)
+        print(seconds, safety_score)
+    print(seconds)'''
 
 def calculate_safety_score(tiles):
     width, height = len(tiles[0]), len(tiles)
-    print(width, height)
-    left_width, right_width = (0, width//2-1), (width//2+1, width-1)
-    upper_height, lower_height = (0, height//2-1), (height//2+1, height-1)
-    print(left_width, right_width)
-    print(upper_height, lower_height)
+    center_x, center_y = width//2, height//2
+    robots_in_quadrants = [0, 0, 0, 0]
+    for i, row in enumerate(tiles):
+        if i < center_y:
+            for j, tile in enumerate(row):
+                if tiles[i][j] != 0:
+                    if j < center_x:
+                        robots_in_quadrants[0] += tiles[i][j]
+                    if j > center_x:
+                        robots_in_quadrants[1] += tiles[i][j]
+        if i > center_y:
+            for j, tile in enumerate(row):
+                if tiles[i][j] != 0:
+                    if j < center_x:
+                        robots_in_quadrants[2] += tiles[i][j]
+                    if j > center_x:
+                        robots_in_quadrants[3] += tiles[i][j]
+    safety_score = 1
+    for robot_count in robots_in_quadrants:
+        safety_score *= robot_count
+    return safety_score
 
 
 def draw_tiles(robots: dict[int, Robot], width: int, height: int):
@@ -40,7 +71,6 @@ def draw_tiles(robots: dict[int, Robot], width: int, height: int):
                     number_of_robots += 1
             row.append(number_of_robots)
         tiles.append(row)
-
     return tiles
 
 
@@ -58,7 +88,6 @@ def move_robot(robot: Robot, width: int, height: int):
     if robot_y_velocity < 0 or robot_x_velocity >= height: robot_y_velocity %= height
     new_robot_x, new_robot_y = (robot_x+robot_x_velocity)%width, (robot_y+robot_y_velocity)%height
     robot.position = new_robot_x, new_robot_y
-    print(robot_x, robot_y, robot_x_velocity, robot_y_velocity, new_robot_x, new_robot_y)
 
 
 def get_robot_data(robots_data):
